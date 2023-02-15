ba35bb48f198e6484416e0dce348c9b2940203f7
akshay-1209 <akshaytonde329@gmail.com>
2023-02-15 22:00:14 +0530

    mongodb

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
@@ -12,8 +15,16 @@ app.get("/resume", function(req, res){
 	res.sendFile(__dirname+"/resume.html");
 });
 
-
-app.listen(port, function(){
-	console.log("Server running on http://localhost:"+port);
-	console.log(`Server running on http://localhost:${port}`);
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
+	if(err){
+		console.error(err);
+	}
+	else{
+		console.log("DB Connected");
+		app.listen(port, function(){
+			console.log("Server running on http://localhost:"+port);
+		});
+	}
 });
+
